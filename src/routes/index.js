e3774a7717827c762e117ebfb3ca4d49fd9fe8d2
tori <ittttori01@gmail.com>
2022-11-02 10:45:13 +0900

    feat : 쿠폰다운로드 api 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const testRouter = require("./testRouter")
+const downloadCouponRouter = require("./couponDownloadRouter");
 
 router.use("/test", testRouter.router)
+
+router.use("/coupon",downloadCouponRouter.router);
 
 module.exports = router;
